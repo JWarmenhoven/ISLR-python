1ed22b6e8e82424d0e282f7a65c6f4b87add3af7
Hongshu Liu <liuhongshu@gmail.com>
2017-10-04 13:52:44 +0100

    plot figure 3.1 and estimate parameters

--- HS_exes/Chapter3.py
+++ HS_exes/Chapter3.py
@@ -16,11 +16,25 @@ pd.set_option('display.notebook_repr_html', False)
 plt.style.use('seaborn-white')
 
 advertising = pd.read_csv('Data/Advertising.csv', usecols=[1,2,3,4])
-advertising.info()
+# advertising.info()
 
 credit = pd.read_csv('Data/Credit.csv', usecols=list(range(1,12)))
 credit['Student2'] = credit.Student.map({'No':0, 'Yes':1})
-print(credit.head(3))
+# print(credit.head(3))
 
 auto = pd.read_csv('Data/Auto.csv', na_values='?').dropna()
-auto.info()
+# auto.info()
+
+sns.regplot(advertising.TV, advertising.Sales, order=1, ci=None, scatter_kws={'color':'r'})
+plt.xlim(-10,310)
+plt.ylim(ymin=0)
+
+
+regr = skl_lm.LinearRegression()
+
+X = scale(advertising.TV, with_mean=True, with_std=False).reshape(-1,1)
+Y = advertising.Sales
+
+regr.fit(X,Y)
+print(regr.intercept_)
+print(regr.coef_)
