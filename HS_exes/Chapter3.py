3c5008dd157e973649b1375a3e96e55e795e3526
Hongshu Liu <liuhongshu@gmail.com>
2017-10-04 14:13:51 +0100

    calculated min RSS

--- HS_exes/Chapter3.py
+++ HS_exes/Chapter3.py
@@ -28,13 +28,28 @@ auto = pd.read_csv('Data/Auto.csv', na_values='?').dropna()
 sns.regplot(advertising.TV, advertising.Sales, order=1, ci=None, scatter_kws={'color':'r'})
 plt.xlim(-10,310)
 plt.ylim(ymin=0)
-
+# plt.show()
 
 regr = skl_lm.LinearRegression()
 
 X = scale(advertising.TV, with_mean=True, with_std=False).reshape(-1,1)
-Y = advertising.Sales
+y = advertising.Sales
 
-regr.fit(X,Y)
+regr.fit(X,y)
 print(regr.intercept_)
 print(regr.coef_)
+
+# Create grid coordinates for plotting
+B0 = np.linspace(regr.intercept_-2, regr.intercept_+2, 50)
+B1 = np.linspace(regr.coef_-0.02, regr.coef_+0.02, 50)
+xx, yy = np.meshgrid(B0, B1, indexing='xy')
+Z = np.zeros((B0.size,B1.size))
+
+#Calculate Z-values (RSS) based on grid of coefficients
+for (i,j),v in np.ndenumerate(Z):
+    Z[i,j] = ((y - (xx[i,j]+X.ravel()*yy[i,j]))**2).sum()/1000
+
+# Minimized RSS
+min_RSS = r'$\beta_0$, $\beta_1$ for minimized RSS'
+min_rss = np.sum((regr.intercept_+regr.coef_*X - y.values.reshape(-1,1))**2)/1000
+print(min_rss)
