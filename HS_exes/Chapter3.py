c0c9343354a5a60d12af736402a576e901a0d6f7
Hongshu Liu <liuhongshu@gmail.com>
2017-10-04 15:48:13 +0100

    test run

--- HS_exes/Chapter3.py
+++ HS_exes/Chapter3.py
@@ -50,6 +50,36 @@ for (i,j),v in np.ndenumerate(Z):
     Z[i,j] = ((y - (xx[i,j]+X.ravel()*yy[i,j]))**2).sum()/1000
 
 # Minimized RSS
-# min_RSS = r'$\beta_0$, $\beta_1$ for minimized RSS'
+min_RSS = r'$\beta_0$, $\beta_1$ for minimized RSS'
 min_rss = np.sum((regr.intercept_+regr.coef_*X - y.values.reshape(-1,1))**2)/1000
 print(min_rss)
+
+fig = plt.figure(figsize=(15,6))
+fig.suptitle('RSS - Regression coefficients', fontsize=20)
+
+ax1 = fig.add_subplot(121)
+ax2 = fig.add_subplot(122, projection='3d')
+
+# left plot
+CS = ax1.contour(xx, yy, Z, cmap=plt.cm.Set1, levels=[2.15, 2.2, 2.3, 2.5, 3])
+ax1.scatter(regr.intercept_, regr.coef_[0], c='r', label=min_RSS)
+ax1.clabel(CS, inline=True, fontsize=10, fmt='%1.1f')
+
+# plt.show()
+
+# right plot
+ax2.plot_surface(xx, yy, Z, rstride=3, cstride=3, alpha=0.3)
+ax2.contour(xx, yy, Z, zdir='z', offset=Z.min(), cmap=plt.cm.Set1, alpha=0.4, levels=[2.15, 2.2, 2.3, 2.5, 3])
+ax2.scatter3D(regr.intercept_, regr.coef_[0], min_rss, c='r', label=min_RSS)
+ax2.set_zlabel('RSS')
+ax2.set_zlim(Z.min(), Z.max())
+ax2.set_ylim(0.02, 0.07)
+
+# Settings common to both plots
+for ax in fig.axes:
+    ax.set_xlabel(r'$\beta_0$', fontsize=17)
+    ax.set_ylabel(r'$\beta_1$', fontsize=17)
+    ax.set_yticks([0.03, 0.04, 0.05, 0.06])
+    ax.legend()
+
+plt.show()
